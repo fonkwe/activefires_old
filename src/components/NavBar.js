b3e70ae318dda825dc323c7145c710a263b7859c
fonkwe <williajeni@gmail.com>
2022-06-21 08:38:39 -0500

    ActiveFire Phase 1

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className="top-nav" id="topest">
+    <h1>ActiveFires Project</h1>
+    <div className="links">
+    <a href="#foot1">About</a>
+    <a href="#yu">Contact</a>
+    </div>
+</div>
+  )
+}
